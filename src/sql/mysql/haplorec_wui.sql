8185af422d52276aceea4a9dc0cbf705a06dfc3a
James Gleeson <james@innovativemedicine.ca>
2013-06-18 18:04:57 -0400

    add a job_state table for tracking the state of targets in a pipeline job

---
+++ src/sql/mysql/haplorec_wui.sql
@@ -0,0 +1,12 @@
+-- Add tables specific to haplorec-wui that are added to the haplorec database 
+-- (i.e. this schema is to be run after haplorec.sql from the haplorec project)
+
+-- Table used for tracking the state of pipeline jobs.  If a target not in this table, then it is 
+-- (implicitly) in a 'not yet running' state. 
+CREATE TABLE job_state (
+    job_id bigint not null,
+    target varchar(200) not null,
+    state enum('running', 'done') not null,
+    foreign key (job_id) references job(id),
+    primary key (job_id, target)
+) ENGINE=InnoDB;

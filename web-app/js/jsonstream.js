3a20075376ddb6dfddaf65e6ccc818eec7fe4634
James Gleeson <james@innovativemedicine.ca>
2013-06-18 12:00:57 -0400

    add comments / pseudocode for implementing jsonstream library

---
+++ web-app/js/jsonstream.js
@@ -0,0 +1,38 @@
+var jsonstream = (function(m) {
+	'use strict';
+
+    m.get = function(url, onMessage) {
+        /* TODO:
+         * read the response of a long-lived asynchronous http request consisting of JSON strings delimited by newlines.
+         * - use XMLHTTPRequest to perform the asynchronous http request
+         *   - possibly use it's on* handlers (e.g. onprogress) to 
+         *   - if using the on* handlers isn't practical (e.g. they don't fire when new messages are 
+         *   received, or they don't fire as frequently as we'd like), set a timer via javascript's 
+         *   setInterval (see: http://friendlybit.com/js/partial-xmlhttprequest-responses/)
+         * - keep track of:
+         *   - start: the position of the start of the next (unread) message in XMLHTTPRequest.responseText (initially 
+         *   0; on reading a message, this should change to position of newline+1)
+         *   - l: the length of data that has been read so far (initially 0)
+         *   - each time we poll XMLHTTPRequest.responseText for changes (regardless of how that might be triggered), do the following:
+         *
+         *   def readMessages() {
+         *      L = XMLHTTPRequest.responseText.length
+         *      while l < L && reponseText[l] != '\n': 
+         *        l += 1
+         *      if l < L: 
+         *        // reponseText[l] == '\n' must be true if we're here, so there's a new message waiting
+         *        message = reponseText.substring(begin, l)
+         *        onMessage(JSON.parse(message))
+         *        // read past newline delimiter 
+         *        l += 1
+         *        // mark the beginning of the next message
+         *        begin = l
+         *        // there might still be messages available to read
+         *        readMessage()
+         *   }
+         *
+         * - on loading new 
+         */
+    };
+
+})(jsonstream || {});
